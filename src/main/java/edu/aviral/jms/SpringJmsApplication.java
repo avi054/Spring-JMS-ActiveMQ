06640ee4fce24712f0a641b96aa5852f742b34ce
Aviral Agarwal <aviralagarwal.2013@gmail.com>
2021-10-10 15:59:37 +0530

    in memory ActiveMQ

--- src/main/java/edu/aviral/jms/SpringJmsApplication.java
+++ src/main/java/edu/aviral/jms/SpringJmsApplication.java
@@ -1,21 +1,37 @@
 package edu.aviral.jms;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.core.JmsTemplate;
 
 @EnableJms
 @SpringBootApplication
-public class SpringJmsApplication {
+public class SpringJmsApplication implements CommandLineRunner {
+	
+	@Value("${ORDER_QUEUE}")
+	private String orderQueue;
+	
+	@Autowired
+	private JmsTemplate jmsTemplate;
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(SpringJmsApplication.class, args);
+//		ConfigurableApplicationContext context = 
+				SpringApplication.run(SpringJmsApplication.class, args);
+//				.close();
 
-		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+//		JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
 
-		jmsTemplate.convertAndSend("order-queue", "Hello World");
+//		jmsTemplate.convertAndSend("order-queue", "Hello World");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		jmsTemplate.convertAndSend(orderQueue, "Hello Again!");
 	}
 
 }
---
+++ src/main/java/edu/aviral/jms/receiver/Receiver.java
@@ -0,0 +1,14 @@
+package edu.aviral.jms.receiver;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+	//determine 'queue name' and on which Message Queue Server(MOM) this queue resides
+	@JmsListener(destination = "${ORDER_QUEUE}", containerFactory = "warehouseFactory")
+	public void receiveMessage(String message) {
+		System.out.println("Order received : " + message);
+	}
+}
